488429894bbff35fec8efd49176e549dab222d15
jahvbogle <jbogle111@proton.me>
2024-12-05 11:24:51 -0500

    Formatting and identified invalid moves

--- main.py
+++ main.py
@@ -4,38 +4,37 @@ board = TicTacToe()
 
 print("(1)Random\n(2)Alpha Beta Pruning\nSelect Level: ", end=" ")
 level = int(input())
+board.display_board()
 
 if level == 1: # Level (1) Random
     while not (board.check_user() or board.check_cpu()):
             # User's turn
-        print('user_x: ', end= " ")
-        user_x = int(input())
-        print('user_y: ', end= " ")
-        user_y = int(input())
-        board.update_user(user_x, user_y)
-            
-            # Computer's turn (Random)
-        random_x = random.randint(0,2)
-        random_y = random.randint(0,2)
-        board.update_cpu(random_x,random_y)
+        print('\nYour turn: \n(row): ', end= " ")
+        row = int(input())
+        print('(column): ', end= " ")
+        column = int(input())
 
-            # Display current state of board
-        current_board = board.get_board()
-        for column in current_board:
-            print(column)
+        board.update_user(row, column)
+        board.display_board()
+           
+            # Computer's turn
+        row = random.randint(0,2)
+        column = random.randint(0,2)
+        board.update_cpu(row,column)
+
+        print("\nCPU's turn (row,column): ({},{})".format(column, row))
+        board.display_board()
 
 if level == 2: # Level (2) Alpha-Beta Pruning
     while not (board.check_user() or board.check_cpu()):
             # User's turn
-        print('user_x: ', end= " ")
-        user_x = int(input())
-        print('user_y: ', end= " ")
-        user_y = int(input())
-        board.update_user(user_x, user_y)
-            
-            #TODO Computer's turn
+        print('(row): ', end= " ")
+        row = int(input())
+        print('(column): ', end= " ")
+        column = int(input())
+        
+        board.update_user(row, column)
+        board.display_board() 
 
-            # Display current state of board
-        current_board = board.get_board()
-        for column in current_board:
-            print(column)
+            #TODO Computer's turn
+            
--- tictactoe.py
+++ tictactoe.py
@@ -4,22 +4,37 @@ class TicTacToe:
                       [" ", " ", " "],
                       [" ", " ", " "]]
         
-     #TODO Check for and prevent collisions
-     def update_user(self, x, y):   
-        self.board[x][y] = 'X'
+     #TODO Prevent collisions
+     def update_user(self, x, y):
+        if not self.board[x][y] == ' ':
+           print("\nInvalid move (User) (Position occupied: '{}' at row={} column={})"
+                 .format(self.board[x][y], x, y))
+        else:
+           self.board[x][y] = 'X'
         return self.board
      
      def update_cpu(self, x, y):
-        self.board[x][y] = 'O'
+        if not self.board[x][y] == ' ':
+           print("\nInvalid move (CPU) (Position occupied: '{}' at row={} column={})"
+                 .format(self.board[x][y], x, y))
+        else:
+           self.board[x][y] = '0'
         return self.board
-
+     
      #TODO Check if goal state is reached
      def check_user(self):
         return not True
      
      def check_cpu(self):
         return not True
-          
+     
      def get_board(self):
         return self.board
-      
+     
+     def display_board(self):
+        print([' ','0', '1', '2'])
+        current_board = self.get_board()
+        i = 0
+        for row in current_board:
+            print(['{}'.format(i)] + row)
+            i+=1
